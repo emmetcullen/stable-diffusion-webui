479913c94549ab7a41277723556e18b3bc9c6ab1
emmetcullen <cullenemmet@gmail.com>
2022-11-25 05:23:46 +0000

    Update webui-user.bat

--- webui-user.bat
+++ webui-user.bat
@@ -5,7 +5,7 @@ pause
 set PYTHON="C:\Users\Emmet\AppData\Local\Programs\Python\Python310\python.exe"
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=--precision full --no-half --lowvram --opt-split-attention
+set COMMANDLINE_ARGS=--precision full --no-half --lowvram --opt-split-attention --xformers
 set ACCELERATE=
 
 call webui.bat
